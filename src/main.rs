121098ef8e2251ec045a16bb66b02cf0cd2990cb
Raj Aryan <rajaryan0x1@gmail.com>
2026-08-03 09:48:02 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,30 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    loop {
-
-    
+    loop {    
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-
+        const BUILTINS: [&str;3] = ["type" , "echo" , "exit"];
         let command = input.trim();
         if command == "exit" {
             break;
         }
+        if command.starts_with("type"){
+            let parts : Vec<&str> = command.split_whitespace().collect();
+            if parts.len() == 2 {
+                let cmd = parts[1];
 
+                if BUILTINS.contains(&cmd) {
+                    println!("{} is a shell builtin",cmd);
+                } else {
+                    println!("{}: not found" , cmd);
+                }
+            }
+
+            continue;
+        }
         if command.starts_with("echo"){
             println!("{}" , &command[5..]);
         }else {
